31ee30bdd6b75110674ed6f35ea3c4a0fe7e814d
VeraKulemina <verakulemina@gmail.com>
2023-11-17 07:29:17 -0700

    anynimus func

--- veriables.ts
+++ veriables.ts
@@ -3,8 +3,15 @@ function square(num: number) {
     return num*num;
 }
 
-function addNums(x: number, y: number) : number {
+function addNums(x: number, y: number) : number | string {
     return x + y;
 }
 
 addNums(5, 5);
+
+const colors = ["red", "green", "yellow"];
+colors.map(color => {
+    return color.toUpperCase();
+})
+
+
