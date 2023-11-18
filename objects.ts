ae8f0e96a1b0aeacccbaa9def22a52d892d18006
VeraKulemina <verakulemina@gmail.com>
2023-11-18 11:40:02 -0700

    intersection type of object

--- objects.ts
+++ objects.ts
@@ -6,3 +6,17 @@
 
  type Point = {x: number, y: number};
  
+
+ //intersection type 
+
+ type Circle = {
+   radius: number;
+ }
+
+ type Combain = Point & Circle;
+
+ const combains = {
+   x: 2,
+   y: 3,
+   radius: 4
+ }
