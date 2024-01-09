b8a361c2e59a6185a352880b83526303d1cc00c1
VeraKulemina <verakulemina@gmail.com>
2024-01-08 19:28:36 -0700

    arrays

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-
-
-
-let highScore = 1500;
-let userScore = 1600;
-
-if (userScore >= highScore) {
-    console.log(`congrats you have new high score of ${userScore}`)
-    highScore = userScore;
-}
-else {
-console.log( "Good Game!");
-}
+let colors =["red", "green", "blue"];
