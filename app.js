fc88ff48e12a9274585d4535d277fe993dd4b033
VeraKulemina <verakulemina@gmail.com>
2024-01-15 19:30:23 -0700

    i love JS

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ colors[colors.length] = "red"
 let numbers = [1,2,3];
 
 let mix = colors.concat(numbers);
+
+
+const fitData = {
+steps: 1500, 
+name: "Vera",
+totalMiles: 1000,
+100: "one hundred"
+}
+
+let mySteps = "steps"
+
+const userReviews = {};
+
+userReviews.name = "Vera";
+
+
+const nums = [1,2,3];
+const mystery = nums;
