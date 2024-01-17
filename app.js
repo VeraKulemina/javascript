503c6ab1b6a08fa49dfa5f9d4995183f616fb25c
VeraKulemina <verakulemina@gmail.com>
2024-01-16 19:32:43 -0700

    while

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
-const examScore = [
-    { 
-        firstName: 'John',
-        score : 89
-    },
-    { 
-        firstName: 'Vera',
-        score : 99
-    },
-    { 
-        firstName: 'Joel',
-        score : 70
-    }      
-]
+let j =0;
 
-for (let i=0; i<examScore.length; i++) {
-    console.log(examScore[i].firstName + ' '+" scored"+ " " + examScore[i].score);
-}
-
-const word ="stressed";
-
-for (let i = word.length -1; i > 0; i--) {
-    console.log(word[i]);
+while(j <=5){
+    console.log(j);
+    j++;
 }
