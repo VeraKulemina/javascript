814a3465881b6d465c86a8f9743f3191718f04f6
VeraKulemina <verakulemina@gmail.com>
2024-01-17 18:27:05 -0700

    more JS

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-let j =0;
 
-while(j <=5){
-    console.log(j);
-    j++;
+
+// for(let i= 0; i <4; i++){
+//     console.log(i);
+//     if(i === 3){
+//         break
+//     }
+// }
+
+const lop = [1,2,3];
+
+for(let item of lop){
+    console.log(item)
 }
