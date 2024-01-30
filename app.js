792bef5355c4dcdb72e2dbbfb2cbfe7f4c30f14b
VeraKulemina <verakulemina@gmail.com>
2024-01-29 20:23:52 -0700

    higher level functions

--- app.js
+++ app.js
@@ -1,74 +1,93 @@
 
 
-// for(let i= 0; i <4; i++){
-//     console.log(i);
-//     if(i === 3){
-//         break
-//     }
-// }
-
-// const lop = [1,2,3];
-
-// for(let item of lop){
-//     console.log(item)
-// }
-
-// for loop and objects
-
-// const movie = {
-//     Arrival: 9.5,
-//     Alien: 9.0,
-//     Amelie: 9
-// }
-
-// // console.log(Object.keys(movie));
-// // console.log(Object.values(movie));
-
-// // for(let m of Object.keys(movie)) {
-// //     console.log(m, movie[m]);
+// // for(let i= 0; i <4; i++){
+// //     console.log(i);
+// //     if(i === 3){
+// //         break
+// //     }
 // // }
 
-// for( let m in movie){
-//     console.log(m);
-// }
+// // const lop = [1,2,3];
 
-// function isPurple(color){
-//     return color.toLowerCase() === 'purple'
+// // for(let item of lop){
+// //     console.log(item)
+// // }
+
+// // for loop and objects
+
+// // const movie = {
+// //     Arrival: 9.5,
+// //     Alien: 9.0,
+// //     Amelie: 9
+// // }
+
+// // // console.log(Object.keys(movie));
+// // // console.log(Object.values(movie));
+
+// // // for(let m of Object.keys(movie)) {
+// // //     console.log(m, movie[m]);
+// // // }
+
+// // for( let m in movie){
+// //     console.log(m);
+// // }
+
+// // function isPurple(color){
+// //     return color.toLowerCase() === 'purple'
         
-// }
+// // }
 
-// function isValidPassword(password, username){
-//     return password.length >= 8 && 
-//         password.indexOf(" ") === -1 && 
-//         password.indexOf(username) === -1
-// }
+// // function isValidPassword(password, username){
+// //     return password.length >= 8 && 
+// //         password.indexOf(" ") === -1 && 
+// //         password.indexOf(username) === -1
+// // }
 
 
-// let sum = 0;
-// let avg = 0
-// function getAvarage(array){
-//     for(let i=0; i<array.length; i++){
-//         sum += array[i];
-//     }
+// // let sum = 0;
+// // let avg = 0
+// // function getAvarage(array){
+// //     for(let i=0; i<array.length; i++){
+// //         sum += array[i];
+// //     }
     
-//     return avg = sum/array.length;
+// //     return avg = sum/array.length;
+// // }
+
+// const alphabetArray = [
+//     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+//     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+//   ];
+
+//   let str = "The quick brown fox jumps over the lazy dog";
+
+// function isPangram(str) {
+//     console.log('hi');
 // }
 
-const alphabetArray = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-  ];
+// function outer() {
+//   let movie = "amadeus";
+//   function inner() {
+//     console.log(movie.toUpperCase())
+//   }
+//   inner();
+// }
 
-  let str = "The quick brown fox jumps over the lazy dog";
-
-function isPangram(str) {
-    console.log('hi');
+function add(a,b) {
+  return a+b
 }
 
-function outer() {
-  let movie = "amadeus";
-  function inner() {
-    console.log(movie.toUpperCase())
-  }
-  inner();
+function divide(a,b) {
+  return a / b
 }
+function sub(a,b) {
+  return a -b
+}
+function multiply(a,b) {
+  return a*b;
+}
+
+const functions = [add, multiply, sub, divide, divide]
+
+functions[1](1, 2) // 2
+
