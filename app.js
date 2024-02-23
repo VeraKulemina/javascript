79a5591281d3cf0ca4cf4cfd7185641c063a428b
VeraKulemina <verakulemina@gmail.com>
2024-02-23 11:50:14 -0700

    all kinds of selectors

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // To select the element with id of bear-photo:
-document.getElementById('bear-photo')
+const img = document.getElementById('bear-photo')
+console.dir(img)
 
 //To select the element with id of main:
 document.getElementById('main')
+
+//getElementsByTagName
+
+const inputs = document.getElementsByTagName("input")
+
+//getElementsByClassName
+
+const header = document.getElementsByClassName("header");
+
+//select first element
+
+const ul = document.getElementsByTagName("ul")[0];
+
+//narroving down the search 
+
+
+const lis = ul.getElementsByClassName("special");
+
+// querySelector
+
+
+
