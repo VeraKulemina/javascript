3fe0a4ab686c087d80dc24fad1fe7cdf4ba23822
VeraKulemina <verakulemina@gmail.com>
2024-03-01 19:24:35 -0700

    NBA Scores Chart Project

--- app.js
+++ app.js
@@ -1,36 +1,85 @@
-const todo = document.querySelector('#todos .todo');
-
-// Setting styles one at a time is not ideal:
-// todo.style.color = 'gray';
-// todo.style.textDecoration = 'line-through';
-// todo.style.opacity = '50%'
-
-
-// We can use a class instead!
-// In app.css I've defined a 'done' class that we can apply
-
-// OPTION 1 - using setAttribute
-//This adds the class 'done', but it overwrites any existing classes...
-// todo.setAttribute('class', 'done');
-
-// OPTION 2 - classList
-// We can use the classList property and it's methods to add,remove, and toggle classes!
-todo.classList.add('done');
-//to remove it
-todo.classList.remove('done');
-//to toggle:
-todo.classList.toggle('done'); //add
-todo.classList.toggle('done'); //remove
-todo.classList.toggle('done'); //add
-todo.classList.toggle('done'); //remove
-todo.classList.toggle('done'); //add
-
-
-//create element with DOM and JavaScript appendChild method
-
-const newH2 = document.createElement('h2');
-
-// prepend adds child to the beggining of parent, append adds child to the end of parent, insertBefore( newElement, elementsThatWillBeBelowNewElement)
-// insertAjaxElement("beforebegin", newElement)
-
-//removeChild; and just .remove 
+const warriorsGames = [{
+    awayTeam: {
+      team: 'Golden State',
+      points: 119,
+      isWinner: true
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 106,
+      isWinner: false
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Golden State',
+      points: 105,
+      isWinner: false
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 127,
+      isWinner: true
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Golden State',
+      points: 126,
+      isWinner: true
+    },
+    awayTeam: {
+      team: 'Houston',
+      points: 85,
+      isWinner: false
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Golden State',
+      points: 92,
+      isWinner: false
+    },
+    awayTeam: {
+      team: 'Houston',
+      points: 95,
+      isWinner: true
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Golden State',
+      points: 94,
+      isWinner: false
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 98,
+      isWinner: true
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Golden State',
+      points: 115,
+      isWinner: true
+    },
+    awayTeam: {
+      team: 'Houston',
+      points: 86,
+      isWinner: false
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Golden State',
+      points: 101,
+      isWinner: true
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 92,
+      isWinner: false
+    }
+  }
+]
